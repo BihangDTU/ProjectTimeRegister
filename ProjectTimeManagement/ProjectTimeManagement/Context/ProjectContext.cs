d9ea8cf5b3104f057af57ca7ca73459309e82e13
Bihang <allandtu1989@gmail.com>
2017-07-27 00:47:40 +0200

    Created a Database

---
+++ ProjectTimeManagement/ProjectTimeManagement/Context/ProjectContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using ProjectTimeManagement.Models;
+
+namespace ProjectTimeManagement.Context
+{
+    public class ProjectContext : DbContext
+    {
+        public DbSet<Project> Projects { get; set; }
+    }
+}
---
+++ ProjectTimeManagement/ProjectTimeManagement/Models/Project.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjectTimeManagement.Models
+{
+    public class Project
+    {
+        public int ProjectId { get; set; }
+        public DateTime ProjectCreatedTime { get; set; }
+        public string ProjectName { get; set; }
+        public string CreatorName { get; set; }
+        public DateTime RegisterTime { get; set; }
+        public float Hours { get; set; }
+    }
+}
